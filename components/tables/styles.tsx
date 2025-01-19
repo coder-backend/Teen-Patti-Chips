427db33e3d72f990d5d721eeb47f2326ca2ddf6c
coder-backend <ashish.adhikari7272gmail.com>
2025-01-19 18:57:02 +0530

    Table Component

---
+++ components/tables/styles.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      padding:20,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+  },
+    table: {
+      width: '100%',
+      height: '100%', 
+      backgroundColor: '#4caf50',
+      borderRadius: 200, 
+      borderWidth: 25,
+      borderTopColor: '#FFD700',
+      borderBottomColor: '#FFD700',
+      borderLeftColor: '#0028FF',
+      borderRightColor: '#0028FF',
+    },
+  });
+  export default styles;
---
+++ components/tables/table.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from './styles';
+const TeenPattiTable = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.table} />
+    </View>
+  );
+};
+
+
+
+export default TeenPattiTable;
